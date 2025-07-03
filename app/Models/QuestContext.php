8e075773f379f5a1d144c64e8a19054eb2cd48cb
Ortadox <misha.vidiakin@gmail.com>
2025-07-03 22:04:24 +0300

    quest_contexts

---
+++ app/Models/QuestContext.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class QuestContext extends Model
+{
+    use HasFactory;
+}
